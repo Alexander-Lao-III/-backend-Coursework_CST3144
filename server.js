3753e139eb39815fc75ba36aedff5c38ebf30e83
Alexander-Lao-III <alexanderlaolll@gmail.com>
2025-11-25 20:51:58 +0400

    added static images

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
+
+app.use("/images", express.static("images"));
 
 let db;
 
