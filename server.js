6109418ec0fd3ab1f556f9cf762064f3ef97f28d
Alexander-Lao-III <alexanderlaolll@gmail.com>
2025-11-25 20:05:15 +0400

    added mongodb atlas connection to backend

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
-const app = express();
+const { MongoClient } = require('mongodb');
 
+const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Backend running');
+let db;
+
+MongoClient.connect(
+  "mongodb+srv://alexander:AlexLaoAlex824@cluster0.dhionr6.mongodb.net/webstore?retryWrites=true&w=majority"
+)
+  .then(client => {
+    db = client.db("webstore");
+    console.log("MongoDB connected");
+  })
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
