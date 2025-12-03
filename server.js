0a81d75c0db046b81c7854760c83bcaaab8fdf1a
Alexander-Lao-III <alexanderlaolll@gmail.com>
2025-12-03 16:27:31 +0400

    updated port for render

--- server.js
+++ server.js
@@ -108,8 +108,9 @@ async function start() {
   });
 
   // server start
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
